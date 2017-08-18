11c415c689fc90266edd95ef63851c8185555bb5
Len <a78965@gmail.com>
2017-08-18 11:35:20 +0800

    Remove getObject() and add environment variables - BRANCH - S3_BUCKET - FILE_NAME

--- app.py
+++ app.py
@@ -10,25 +10,15 @@ from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
 from botocore.compat import urlsplit
 
+REGION = os.getenv('AWS_REGION')
+S3_BUCKET = os.getenv('S3_BUCKET', '')
+FILE_NAME = os.getenv('FILE_NAME', 'repo.zip')
+BRANCH = os.getenv('BRANCH', 'master')
 
 s3 = boto3.client('s3')
-codecommit = boto3.client('codecommit', region_name='ap-northeast-1')
+codecommit = boto3.client('codecommit', region_name=REGION)
 session = boto3.Session()
 
-def getObject(bucket, objectKey):
-    try:
-        object = s3.get_object(Bucket=bucket, Key=objectKey)
-        if 'Body' in object:
-            return object['Body'].read()
-        else:
-            print 'No content in the get object response.'
-            return False
-    except ClientError as exception:
-        print exception.response
-        if exception.response['Error']['Code'] == '404':
-            return False
-        else:
-            return None
 
 def sign_request(region, url_to_sign):
     credentials = session.get_credentials()
@@ -45,24 +35,22 @@ def sign_request(region, url_to_sign):
         request.method,
         split.path,
         hostname)
-    print 'CanonicalRequest:\n%s', canonical_request
+    print 'CanonicalRequest:\n%s' % canonical_request
     string_to_sign = signer.string_to_sign(request, canonical_request)
-    print 'StringToSign:\n%s', string_to_sign
+    print 'StringToSign:\n%s' % string_to_sign
     signature = signer.signature(string_to_sign, request)
-    print 'Signature:\n%s', signature
+    print 'Signature:\n%s' % signature
 
     return '{0}Z{1}'.format(request.context['timestamp'], signature)
 
             
-def get_cred(theUrl):
+def get_cred(repo_url, repo_region):
     credentials = session.get_credentials()
     credentials = credentials.get_frozen_credentials()
 
     username = credentials.access_key + '%' + credentials.token
-    # secret_access_key = credentials.secret_key
 
-    region = 'ap-northeast-1'
-    password = sign_request(region, theUrl)
+    password = sign_request(repo_region, repo_url)
 
     cred = pygit2.UserPass(username, password)
 
@@ -70,29 +58,24 @@ def get_cred(theUrl):
 
     return RemoteCallbacks(credentials=cred)
 
-def clone(repo_url, repo_path):
+def clone(repo_url, repo_region, repo_path):
     print repo_url
+    print repo_region
     print repo_path
 
-    repo = pygit2.clone_repository(repo_url, repo_path, callbacks=get_cred(repo_url)) # Clones a non-bare repository
+    repo = pygit2.clone_repository(repo_url, repo_path, checkout_branch=BRANCH, callbacks=get_cred(repo_url, repo_region))
 
-def Achive_Folder_To_ZIP(sFilePath, dest = ""):
-    """
-    input : Folder path and name
-    output: using zipfile to ZIP folder
-    """
+def zipRepository(repo_path, dest = ""):
     if (dest == ""):
-        zf = zipfile.ZipFile(sFilePath + '.ZIP', mode='w')
+        zf = zipfile.ZipFile(repo_path + '.zip', mode='w')
     else:
         zf = zipfile.ZipFile(dest, mode='w')
  
-    os.chdir(sFilePath)
-    #print sFilePath
+    os.chdir(repo_path)
     for root, folders, files in os.walk("."):
-        for sfile in files:
-            aFile = os.path.join(root, sfile)
-            #print aFile
-            zf.write(aFile)
+        for file in files:
+            file_path = os.path.join(root, file)
+            zf.write(file_path)
     zf.close()
 
 def initialize():
@@ -103,6 +86,10 @@ def initialize():
             shutil.rmtree(os.path.join(root, d))
 
 def lambda_handler(event, context):
+
+    if S3_BUCKET == '' or S3_BUCKET == None:
+        return 'Please specify the S3_BUCKET value in the environment variable.'
+
     # Log the updated references from the event
     references = { reference['ref'] for reference in event['Records'][0]['codecommit']['references'] }
     print("References: "  + str(references))
@@ -110,18 +97,23 @@ def lambda_handler(event, context):
     # Get the repository from the event and show its git clone URL
     repository = event['Records'][0]['eventSourceARN'].split(':')[5]
     try:
+        # Clear the /tmp if the lambda is having concurrency requests
         initialize()
     
         response = codecommit.get_repository(repositoryName=repository)
         cloneUrlHttp = response['repositoryMetadata']['cloneUrlHttp']
-        repo_path = '/tmp/plumber'
-        dest = '/tmp/plumber.zip'
 
-        clone(cloneUrlHttp, repo_path)
-        Achive_Folder_To_ZIP(repo_path, dest)
+        # Generate repository path by time
+        repo_path = os.path.join('/tmp', datetime.datetime.now().strftime('%H%M%S%f'))
+        dest = os.path.join('/tmp', FILE_NAME)
+
+        clone(cloneUrlHttp, REGION, repo_path)
+        zipRepository(repo_path, dest)
         
         if (os.path.isfile(dest)):
-            s3.upload_file(dest, 'plumber-public', 'plumber.zip')
+            s3.upload_file(dest, S3_BUCKET, FILE_NAME)
+        else:
+            return 'No compressed file exists'
         
     except Exception as e:
         print(e)
